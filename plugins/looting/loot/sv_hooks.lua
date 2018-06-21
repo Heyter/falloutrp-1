eed6cae45f4d5ca51c08f85f2878410311f33b64
Cat.jpeg <supermicronde3@gmail.com>
2018-06-21 07:05:21 +0200

    Update sv_hooks.lua

--- plugins/looting/loot/sv_hooks.lua
+++ plugins/looting/loot/sv_hooks.lua
@@ -1,10 +1,7 @@
 local PLUGIN = PLUGIN
 
-
---[[ Loot creation ]]--
-
+-- Create a corpse when a player die and transfer his loot in it
 function PLUGIN:OnCorpseCreated(corpse, victim, char)
-
 	local victimInventory = char:getInv()
 	local victimMoney = char:getMoney()
 
@@ -16,37 +13,35 @@ function PLUGIN:OnCorpseCreated(corpse, victim, char)
 
 	PLUGIN:TransferMoney(victim, corpse)
 
-	function corpse:LootThink()
+	-- Check that players are near and looking the corpses
+	function corpse:EyeTraceCheck()
 		if ( CurTime() < (self.NextTraceCheck or 0) ) then return end
 
 		if ( self.Searchers ) then
 			for k, _ in pairs(self.Searchers) do
 				if ( PLUGIN:EyeTrace(k) ~= self ) then
-					PLUGIN:CloseCorpse(k, false)
+					PLUGIN:CloseLoot(k, false)
 				end
 			end
 		end
 		
 		self.NextTraceCheck = CurTime() + 0.1
 	end
-	hook.Add("Think", corpse, corpse.LootThink)
-
+	hook.Add("Think", corpse, corpse.EyeTraceCheck)
 end
 
+-- Attach a new inventory to a corpse
 function PLUGIN:CreateInventory(corpse, width, height)
-
 	local inv
 	nut.item.newInv(0, "corpse", function(instance)
-
 		instance.w = width
 		instance.h = height
 		inv = instance
-
 	end)
 
 	corpse:SetVar("LootInv", inv)
+	-- Remove inventory from db when the corpse is removed
 	corpse:CallOnRemove("RemoveLootInv", function(ent)
-
 		local inv = ent:GetVar("LootInv")
 		local invId = inv:getID()
 
@@ -55,89 +50,81 @@ function PLUGIN:CreateInventory(corpse, width, height)
 			nut.db.query("DELETE FROM nut_items WHERE _invID = " .. invId)
 			nut.db.query("DELETE FROM nut_inventories WHERE _invID = " .. invId)
 		end
-
 	end)
 
 	return inv
-
 end
 
+-- Transfer inventory items to another
 function PLUGIN:TransferInventory(from, to)
+	if ( not (from and to) ) then return end
+	
+	local fromSlots = from.slots
+	local fromChar = nut.char.loaded[from.owner]
+	local fromPlayer = fromChar:getPlayer()
 
-	if ( from && to ) then
-		
-		local fromSlots = from.slots
-		local fromChar = nut.char.loaded[from.owner]
-		local fromPlayer = fromChar:getPlayer()
+	local toSlots = to.slots
 
-		local toSlots = to.slots
+	if ( fromSlots ) then
+		for x, items in pairs(fromSlots) do
+			for y, item in pairs(items) do
 
-		if ( fromSlots ) then
-			for x, items in pairs(fromSlots) do
-				for y, item in pairs(items) do
+				if ( fromPlayer and item:getData("equip") ) then
+					item:call("EquipUn", fromPlayer)
+				end
 
-					if ( fromPlayer and item:getData("equip") ) then
-						item:call("EquipUn", fromPlayer)
-					end
-
-					if ( not toSlots[x] ) then
-						toSlots[x] = {}
-					end
+				if ( not toSlots[x] ) then
+					toSlots[x] = {}
+				end
 						
-					toSlots[x][y] = item
-					fromSlots[x][y] = nil
+				toSlots[x][y] = item
+				fromSlots[x][y] = nil
 					
-				end
 			end
-
-			-- Sync victim's character inventory
-			for k, v in ipairs(fromChar:getInv(true)) do
-				if (type(v) == "table") then 
-					v:sync(fromPlayer)	
-				end
-			end
-				
 		end
 
+		-- Sync victim's character inventory
+		for k, v in ipairs(fromChar:getInv(true)) do
+			if (type(v) == "table") then 
+				v:sync(fromPlayer)	
+			end
+		end
+				
 	end
-
 end
 
+-- Transfer the money from a character to another
 function PLUGIN:TransferMoney(victim, corpse)
-
 	local char = victim:getChar()
 	local money = char:getMoney()
 
 	corpse:SetVar("LootMoney", money)
 	char:setMoney(0)
-
 end
 
 --[[ Corpse opening ]]--
 
+-- Make know that a player is currently seaching the corpse
 function PLUGIN:RegSearcher(corpse, client)
-
 	if ( not corpse.Searchers ) then
 		corpse.Searchers = {}
 	end
 	corpse.Searchers[client] = true
 
 	client:SetVar("LootCorpse", corpse)
-
 end
 
+-- Make know that a player is no longer seaching the corpse
 function PLUGIN:UnregSearcher(corpse, client)
-
 	if ( corpse.Searchers ) then
 		corpse.Searchers[client] = nil
 	end
 
 	client:SetVar("LootCorpse", nil)
-
 end
 
-function PLUGIN:CloseCorpse(client, share)
-
+-- Close corpse loot
+function PLUGIN:CloseLoot(client, share)
 	local corpse = client:GetVar("LootCorpse")
 
 	if ( IsValid(corpse) ) then
@@ -149,15 +136,14 @@ function PLUGIN:CloseCorpse(client, share)
 		end
 
 	end
-
 end
 
 netstream.Hook("lootExit", function(client)
-	PLUGIN:CloseCorpse(client)
+	PLUGIN:CloseLoot(client)
 end)
 
-function PLUGIN:OpenCorpse(corpse, client)
-
+-- Open corpse loot
+function PLUGIN:OpenLoot(corpse, client)
 	if ( IsValid(corpse) ) then
 		local inv = corpse:GetVar("LootInv")
 
@@ -169,12 +155,10 @@ function PLUGIN:OpenCorpse(corpse, client)
 			netstream.Start(client, "lootOpen", inv:getID(), corpse:GetVar("LootMoney"))
 		end
 	end
-
 end
 
 -- Stared action to open the inventory of a corpse
 netstream.Hook("lootOpen", function(client)
-
 	if ( not IsValid(client) ) then return end
 
 	local corpse = PLUGIN:EyeTrace(client)
@@ -185,11 +169,10 @@ netstream.Hook("lootOpen", function(client)
 		client:doStaredAction(corpse, function() 
 
 			if ( IsValid(corpse) ) then
-				PLUGIN:OpenCorpse(corpse, client)
+				PLUGIN:OpenLoot(corpse, client)
 			end
 
 		end, 1, function()
-
 			if ( IsValid(client) ) then
 				client:setAction()
 			end
@@ -197,42 +180,34 @@ netstream.Hook("lootOpen", function(client)
 		end, PLUGIN.corpseMaxDist)
 
 	end
-
 end)
 
 
 --[[ Money management ]]--
 
+-- Send corpse money to all corpse searchers
 function PLUGIN:ShareCorpseMoney(corpse)
-
 	local searchers = corpse.Searchers
-
+	
 	if ( searchers ) then
-
 		netstream.Start(searchers, "lootMoney", corpse:GetVar("LootMoney"))
-
 	end
-
 end
 
+-- Widthdraw money from loot reserve
 function PLUGIN:WidthdrawMoney(client, corpse, amount)
-
 	local oldCorpseMoney = corpse:GetVar("LootMoney")
-
+	
 	if ( amount <= oldCorpseMoney ) then
-
 		corpse:SetVar("LootMoney", oldCorpseMoney - amount)
 		PLUGIN:ShareCorpseMoney(corpse)
 
 		local char = client:getChar()
 		char:giveMoney(amount)
-
 	end
-
 end
 
 netstream.Hook("lootWdMny", function(client, amount)
-
 	if ( not isnumber(amount) ) then return end
 	if ( not IsValid(client) ) then return end
 
@@ -240,28 +215,23 @@ netstream.Hook("lootWdMny", function(client, amount)
 	if ( not IsValid(corpse) ) then return end
 
 	PLUGIN:WidthdrawMoney(client, corpse, amount)
-
 end)
 
+-- Deposit money to corpse reserve
 function PLUGIN:DepositMoney(client, corpse, amount)
-
 	local char = client:getChar()
 	local oldCharMoney = char:getMoney()
 
 	if ( amount <= oldCharMoney ) then
-
 		local oldCorpseMoney = corpse:GetVar("LootMoney")
 		corpse:SetVar("LootMoney", oldCorpseMoney + amount)
 		PLUGIN:ShareCorpseMoney(corpse)
 
 		char:takeMoney(amount)
-
 	end
-
 end
 
 netstream.Hook("lootDpMny", function(client, amount)
-
 	if ( not isnumber(amount) ) then return end
 	if ( not IsValid(client) ) then return end
 
@@ -269,5 +239,4 @@ netstream.Hook("lootDpMny", function(client, amount)
 	if ( not IsValid(corpse) ) then return end
 
 	PLUGIN:DepositMoney(client, corpse, amount)
-
 end)
