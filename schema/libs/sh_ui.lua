f2994f40818474974c651e639f8110eb661d1326
Cat.jpeg <supermicronde3@gmail.com>
2018-06-24 00:26:45 +0200

    Wrapping text drawing functions

--- schema/libs/sh_ui.lua
+++ schema/libs/sh_ui.lua
@@ -1,4 +1,4 @@
-falloutui = {}
+fo_ui = fo_ui or {} -- Fuck you vin
 
 function SCHEMA:GetColor(alpha)
     local color = self.ThemeColor
@@ -18,36 +18,33 @@ FALLOUT_BLUE = Color(46, 207, 255, 255)
 FALLOUT_GREEN = Color(26, 255, 128, 255)
 FALLOUT_WHITE = Color(192, 255, 255 ,255)
 
-
 --Helper UI functions
 
-function falloutui.DrawBlur(x,y,text,blur,color,font,blurfont)
-    local alpha = alpha or 255
+function fo_ui.WrapText(text, font, maxWidth)
+	local words = string.Explode( " ", text )
+	
+	local lines = {}
+	local index = 1
+	lines[index] = ""
+	
+	surface.SetFont(font)
+	
+	for _, word in pairs(words) do
+		if ( surface.GetTextSize(line..word) < maxWidth ) then
+			lines[index] = lines[index]..word
+		else
+			index = index + 1
+			lines[index] = word
+		end
+	end
+	
+	return lines or {}
+end
 
-    if blur == nil then 
-        blur = false 
-    end
-
-    if color and color != 0 then
-        color = color
-    else
-        color = SCHEMA:GetColor()
-    end
-
-    local normfo, blurfo = "Monofonto24", "Monofonto24_blur"
-
-    if (font and blurfont) then
-        normfo = font
-        blurfo = blurfont
-    end
-    
-    surface.SetFont(blurfo)
-	surface.SetTextColor(color)
-    for i =0,3 do
-      	surface.SetTextPos(x, y)
-        surface.DrawText(text)
-    end
-    surface.SetFont(normfo)
-    surface.SetTextPos(x, y)
-    surface.DrawText(text)
+function fo_ui.DrawWrappedText(lines)
+	if ( not lines ) then return end
+	
+	for _, line in pairs(lines) do
+		surface.DrawText(line)
+	end
 end
