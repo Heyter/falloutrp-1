82e7c77ae8200a453ab0fbdb4834dd3231d412ac
supermicronde3 <supermicronde3@gmail.com>
2018-06-24 22:18:40 +0200

    Added WrapText function

--- schema/libs/sh_string.lua
+++ schema/libs/sh_string.lua
@@ -1,60 +1,24 @@
 fo.string = fo.string or {}
 
+-- surface
+local setFont = surface.SetFont
+local getTextSize = surface.GetTextSize
+-- string
+local strSub = string.sub
+local strLen = string.len
+-- math
+local mathCeil = math.ceil
+-- table
+local tblInsert = table.insert
+local tblConcat = table.concat
+
 function fo.string.Explode( seperators, str )
-	local p = "[^"..table.concat(seperators).."]+"
+	local p = "[^"..tblConcat(seperators).."]+"
 
 	local words = {}
 	for w in str:gmatch(p) do
-		table.insert(words, w)
+		tblInsert(words, w)
 	end
 
 	return words
-end
-
-function fo.string.WrapText( text, font, wLimit, w )
-	w = w or math.ceil(string.len( text ) / 2)
-	
-	local words = fo.string.Explode( {" ", "\n"}, text )
-	local lastWord
-
-	local lines = {}
-	local index = 1
-
-	local function newLine()
-		index = index + 1
-	end
-
-	local function setLine(line)
-		lines[index] = line
-	end
-
-	local function appendLine(word)
-		local line = lines[index]
-
-		if ( line ) then
-			return line.." "..word
-		else
-			return word
-		end
-	end
-
-	surface.SetFont(font)
-	local charW = surface.GetTextSize("A")
-
-	for _, w in pairs(words) do
-		local futureLine = appendLine(w)
-
-		if ( surface.GetTextSize(futureLine) > wLimit ) then
-			if ( surface.GetTextSize(w) < w ) then
-				setLine(futureLine)
-			else
-				for i = string.len( w ), 1, -1 do
-					--local piece = 
-				end
-
-			end
-		end
-
-		lastWord = v
-	end
 end
