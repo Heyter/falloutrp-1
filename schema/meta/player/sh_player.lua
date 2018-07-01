1851b2820a8359d92ac1fe159b2f2e7435f33f1b
supermicronde3 <supermicronde3@gmail.com>
2018-07-01 23:01:00 +0200

    Added parentheses

--- schema/meta/player/sh_player.lua
+++ schema/meta/player/sh_player.lua
@@ -1,22 +1,22 @@
 local Player = FindMetaTable("Player")
 
-if CLIENT then
+if ( CLIENT ) then
 	local forpNotifyQueue = forpNotifyQueue or {}
 
 	local function displayNotify(message, state, colour)
 		local notify = vgui.Create("forpNotify")
 		notify:SetMessage(message)
-		if state then
+		if ( state ) then
 			notify:SetState(state) -- Sets icon 
 		end
-		if colour then
+		if ( colour ) then
 			notify:SetColor(colour)
 		end
 		surface.PlaySound("forp/notify.mp3") -- Play dat bing
 		timer.Simple(7, function() 
 			table.remove(forpNotifyQueue, 1) -- Remove from table
 
-			if forpNotifyQueue[1] then
+			if ( forpNotifyQueue[1] ) then
 				notify:Remove()
 				local nextNotify = forpNotifyQueue[1] -- Get what notification is next in line
 				displayNotify(nextNotify[1], (nextNotify[2] or nil), (nextNotify[3] or nil)) -- Display it
@@ -30,7 +30,7 @@ if CLIENT then
 	function pushNotify(message, state, colour)
 		table.insert(forpNotifyQueue, {message, (state or nil), (colour or nil)}) -- Put in queue
 
-		if #forpNotifyQueue == 1 then -- If nothing is in front; display
+		if ( #forpNotifyQueue == 1 ) then -- If nothing is in front; display
 			return displayNotify(message, (state or nil), (colour or nil))
 		end
 	end
@@ -46,7 +46,7 @@ function nut.util.notify(message, client) -- Hijack the nutscript notification s
 end
 
 function Player:foNotify(message, state, colour) -- State can be: normal, sad, caps, lock, gift, ncr, legion, bos, key, map, pain and radio | Colour will auto set based on clients ui colour
-	if SERVER then
+	if ( SERVER ) then
 		netstream.Start(self, "foNotify", message, (state or nil), (colour or nil))
 	else
 		pushNotify(message, (state or nil), (colour or nil))
@@ -54,12 +54,13 @@ function Player:foNotify(message, state, colour) -- State can be: normal, sad, c
 end
 
 function Player:foNotifyLocalized(message, translateCodes, state, colour) -- Translate codes is a table
-	if SERVER then
+	if ( SERVER ) then
 		netstream.Start(self, "foNotifyLocalized", message, translateCodes, (state or nil), (colour or nil))
 	else
 		pushNotify(L(message, unpack(translateCodes)), (state or nil), (colour or nil))
 	end
 end
+
 
 function Player:GetLookedEntity(dist)
     local d = {}
@@ -68,4 +69,53 @@ function Player:GetLookedEntity(dist)
     d.endpos = data.start + self:GetAimVector()*dist
 
     return util.TraceLine(d).Entity
+end
+
+
+function Player:FreezeMove(state, share)
+	self:SetVar("movementsFreeze", state)
+	
+	if ( SERVER and share ) then
+		netstream.Start(self, "foFreezeMov", state)
+	end
+end
+
+local allowCommand
+hook.Add("StartCommand", "FreezeExceptInputs1", function(ply, cmd)
+    if ( ply:GetVar("movementsFreeze") ) then
+        cmd:SetButtons(0)
+    end
+end)
+
+hook.Add("Move", "FreezeExceptInputs2", function(ply, mvd)
+    if ( ply:GetVar("movementsFreeze") ) then
+        return true
+    end
+end)
+
+if ( CLIENT ) then
+	netstream.Hook("foFreezeMov", function(state)
+		LocalPlayer():FreezeMove(state)
+	end)
+end
+
+
+function Player:FreezeWeapon(state, share)
+	self:SetVar("weaponFreeze", state)
+	
+	if ( SERVER and share ) then
+		netstream.Start(self, "foFreezeWep", state)
+	end
+end
+
+hook.Add("PlayerSwitchWeapon", "lpRstrictSwitch", function(ply, oldWep, newWep)
+	if ( ply:GetVar("weaponFreeze") ) then
+        return true
+    end
+end)
+
+if ( CLIENT ) then
+	netstream.Hook("foFreezeWep", function(state)
+		LocalPlayer():FreezeWeapon(state)
+	end)
 end
