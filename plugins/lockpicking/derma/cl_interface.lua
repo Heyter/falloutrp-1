439605d5c40d9eefbab2dad52b5fc80a928bac7c
supermicronde3 <supermicronde3@gmail.com>
2018-07-07 10:03:25 +0200

    Fixed DrawTexturedRectRotatedPoint

--- plugins/lockpicking/derma/cl_interface.lua
+++ plugins/lockpicking/derma/cl_interface.lua
@@ -1,35 +1,27 @@
 local PLUGIN = PLUGIN
-PLUGIN.res = PLUGIN.res or {}
-local res = PLUGIN.res
+PLUGIN.res = PLUGIN.res or {} ; local res = PLUGIN.res
+PLUGIN.session = PLUGIN.session or {} ; local session = PLUGIN.session
+PLUGIN.config = PLUGIN.config or {} ; local cfg = PLUGIN.config
 
 local INTERFACE = {}
 INTERFACE.borderW = 448
 INTERFACE.borderH = 197
 INTERFACE.Sounds = {}
 
+local EXIT_KEY = KEY_E
+local TURN_KEY = KEY_A
+
 function INTERFACE:OnInit()
 	PLUGIN.Interface = self
 
-	local exitBtn = self:AddButton("Exit E)")
-	local self = self
-    exitBtn.DoClick = function(this)
-        self:Remove()
+	self.exitBtn = self:AddButton("Exit E)")
+    self.exitBtn.DoClick = function(this)
+        PLUGIN:CloseInterface()
     end
 
 	self:AddInfoTab("Lockpick Skill:", "32")
 	self:AddInfoTab("Bobby Pins:", "15")
 	self:AddInfoTab("Lock Level:", "Very Easy")
-	
-    self:AddButton("Force Lock [42%] F)")
-end
-
-function surface.DrawTexturedRectRotatedPoint( x, y, w, h, rot, x0, y0 )
-	local c = math.cos( math.rad( rot ) )
-	local s = math.sin( math.rad( rot ) )
-	local newx = y0 * s - x0 * c
-	local newy = y0 * c + x0 * s
-		
-	surface.DrawTexturedRectRotated( x + newx, y + newy, w, h, rot )
 end
 
 local matLockinside = Material( "vgui/fallout/lockpicking/inner.png" )
@@ -124,49 +116,54 @@ function INTERFACE:DrawMain()
 			PLUGIN.LastPickMove = CurTime()
 
 			if (CurTime() > PLUGIN.NextPickSound) then
-				PLUGIN.NextPickSound = CurTime() + math.Rand(0.5, 1)
+				session.NextPickSound = CurTime() + math.Rand(0.5, 1)
 				PLUGIN:PlaySound("lockpicking/pickmovement_"..math.random(13)..".wav", 50, 1)
 			end
 				
-			PLUGIN.OldPinRotation = PLUGIN.PinAngle
+			session.OldPinRotation = session.PinAngle
 		end
 
 	end
-	if (not PLUGIN.ChangingPin) then
+	if (not session.ChangingPin) then
 		surface.SetDrawColor( 200, 200, 200, 255 )
 		surface.SetMaterial( matLockpick )
-		surface.DrawTexturedRectRotatedPoint( res.sW / 2, res.sH / 2, res.pickW, res.pickH, 180 - PLUGIN.PinAngle, res.pickW / 2, 0 )
+		fo.ui.DrawTexturedRectRotatedPoint( res.sW / 2, res.sH / 2, res.pickW, res.pickH, 180 - PLUGIN.PinAngle, res.pickW / 2, 0 )
 	end
 
 	nut.bar.drawAction()
 end
 
 function PLUGIN:PlayerButtonDown(ply, btn)
-	if (btn == KEY_A) then
-		if (self.LockAngle ~= 0) then return end
-		if (self.Success) then return end
-		if (self.ChangingPin) then return end
-		if (self.LastRotating and CurTime() - self.LastRotating < self.SpamTime + 0.08) then return end
+	local interface = self.Interface
+	if ( not IsValid(interface) ) then return end
 
-		netstream.Start("lpRotat", true, self.PinAngle)
+	if (btn == TURN_KEY) then
+		if (session.LockAngle ~= 0) then return end
+		if (session.Success) then return end
+		if (session.ChangingPin) then return end
+		if (session.LastRotating and CurTime() - session.LastRotating < cfg.SpamTime + 0.08) then return end
 
-		self.LastRotating = CurTime()
-		self.RotatingLock = true
-	elseif (btn == KEY_Q) then
-		netstream.Start("lpStop")
-		self:CloseInterface()
+		netstream.Start("lpRotat", true, session.PinAngle)
+
+		session.LastRotating = CurTime()
+		session.RotatingLock = true
+	elseif (btn == EXIT_KEY) then
+		interface.exitBtn:DoClick()
 	end
 end
 
 function PLUGIN:PlayerButtonUp(ply, btn)
-	if (btn == KEY_A) then
-		if (not self.RotatingLock) then return end
-		if (self.Success) then return end
-		if (self.ChangingPin) then return end
+	local interface = self.Interface
+	if ( not IsValid(interface) ) then return end
+
+	if (btn == TURN_KEY) then
+		if (not session.RotatingLock) then return end
+		if (session.Success) then return end
+		if (session.ChangingPin) then return end
 
 		netstream.Start("lpRotat", false)
 
-		self.RotatingLock = false
+		session.RotatingLock = false
 	end
 end
 
