efa459b28c6f5fad7ebef01fac601589d8f98646
supermicronde3 <supermicronde3@gmail.com>
2018-07-09 00:35:04 +0200

    Moved resources to sh_schema.lua

--- schema/sh_schema.lua
+++ schema/sh_schema.lua
@@ -1,10 +1,6 @@
 --------------
 --[[ INFO ]]--
 --------------
-<<<<<<< HEAD
-=======
-
->>>>>>> 5eb62f94f06c574a0a656a216ca9b63fa605e1be
 SCHEMA.name = "Fallout: New Vegas"
 SCHEMA.author = "SuperMicronde, vin, Trip, Otunga"
 SCHEMA.desc = "An official NutScript schema"
@@ -12,10 +8,6 @@ SCHEMA.desc = "An official NutScript schema"
 -----------------------
 --[[ GLOBAL TABLES ]]--
 -----------------------
-<<<<<<< HEAD
-=======
-
->>>>>>> 5eb62f94f06c574a0a656a216ca9b63fa605e1be
 FO_CLTHG = FO_CLTHG or {} -- Clothing
 FO_CHRGUI = FO_CHRGUI or  {} -- Char menu
 FO_MRGNG = FO_MRGNG or {} -- Merging
@@ -29,14 +21,6 @@ clothes = clothes or {} -- Clothing
 ------------------
 --[[ INCLUDES ]]--
 ------------------
-<<<<<<< HEAD
-=======
-
->>>>>>> 5eb62f94f06c574a0a656a216ca9b63fa605e1be
-nut.util.includeDir("libs", nil, true)
-nut.util.includeDir("meta", nil, true)
-nut.util.includeDir("modules", nil, true)
-nut.util.includeDir("derma", nil, true)
 nut.util.includeDir("hooks", nil, true)
 
 nut.util.include("resources.lua", "server")
@@ -45,10 +29,6 @@ nut.util.include("fonts.lua", "client")
 -----------------------
 --[[ CONFIGURATION ]]--
 -----------------------
-<<<<<<< HEAD
-=======
-
->>>>>>> 5eb62f94f06c574a0a656a216ca9b63fa605e1be
 nut.currency.set("","Cap", "Caps")
 
 SCHEMA:DisablePlugin("doors")
@@ -57,12 +37,9 @@ SCHEMA:DisablePlugin("storage")
 
 SCHEMA:OverrideConfig("color", forp_amber)
 SCHEMA:OverrideConfig("font", "Monofonto")
-<<<<<<< HEAD
 
 ---------------
 --[[ FILES ]]--
 ---------------
 resource.AddWorkshop( "891790188" ) -- Fallout 3 Custom Backpacks
 resource.AddWorkshop( "203873185" ) -- Fallout Collection: Aid Props
-=======
->>>>>>> 5eb62f94f06c574a0a656a216ca9b63fa605e1be
